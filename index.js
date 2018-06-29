cd07707e5a93792ed796d0fab93ba2a02874107a
Michael Lok <michaellok34@yahoo.co.uk>
2018-06-29 14:18:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var uppercase = "YES INDEED";
+  var uppercase = "YES INDEED!";
   uppercase.toUpperCase() === string;
   
   return string; 
